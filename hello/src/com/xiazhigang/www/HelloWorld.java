c14caf8c611300f58c1da313d2a1f94b44f0c9b0
xiazhigang <305954240@qq.com>
2020-12-20 16:04:17 +0800

    test connection remote .

---
+++ hello/src/com/xiazhigang/www/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.xiazhigang.www;
+
+import java.util.Date;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World "+ new Date());
+
+	}
+
+}
